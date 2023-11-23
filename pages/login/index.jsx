afe8640d8adc9e7d0fe18ec85d59ac778c761aae
elisheba-beep <ekunife35@gmail.com>
2023-11-23 14:12:41 +0100

    add login and signup screens

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,9 +1,58 @@
 import AuthLayout from "@/components/layouts/AuthLayout/AuthLayout";
+import styles from "./login.module.scss";
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
+import { Routes } from "@/constants/navigation";
 
 function LogInRight() {
-  return <div>index</div>;
+  
+  return (
+    <div className={styles.signUpContainer}>
+      <div>
+        <h3>Sign In</h3>
+        <p>Log in to your account to get started!</p>
+      </div>
+
+      <form>
+        {/* input */}
+
+        <div className={styles.inputContainer}>
+          <label>Email or Phone Number</label>
+          <input placeholder="Enter your email or phone number" type="text" />
+        </div>
+        {/* password */}
+        <div className={styles.passwordContainer}>
+          <label>Password</label>
+          <div className={styles.passwordInput}>
+            <input placeholder="Enter password" type="password" />
+            <Image
+              src={"/images/eye.svg"}
+              alt="visibility"
+              width={24}
+              height={24}
+            />
+          </div>
+        </div>
+        <Link href={""} className={styles.forgotPassword}>
+          Forgot Password?
+        </Link>
+        {/* submit button */}
+        <button className={styles.submitButton}>
+          <p>Sign In</p>
+        </button>
+      </form>
+
+      {/* have an account? */}
+      <div className={styles.haveAnAccount}>
+        <p>I don&apos;t have an account yet? </p>
+        <Link href={Routes.Signup} className={styles.loginText}>
+          Sign Up
+        </Link>
+      </div>
+    </div>
+  );
 }
 
 export default function Login() {
--- pages/signup/index.jsx
+++ pages/signup/index.jsx
@@ -1,14 +1,74 @@
 import AuthLayout from "@/components/layouts/AuthLayout/AuthLayout";
+import styles from "./Signup.module.scss";
 import Head from "next/head";
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Routes } from "@/constants/navigation";
 
 function SignupRight() {
   return (
-    <div>
+    <div className={styles.signUpContainer}>
+      <div>
         <h3>Sign Up</h3>
-        <p>Register with us to get started</p>
+        <p>Register with us to get started!</p>
+      </div>
+
+      <form>
+        {/* input */}
+        <div className={styles.inputContainer}>
+          <label>Full Name</label>
+          <input placeholder="Enter full name" type="text" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Email Address</label>
+          <input placeholder="Enter email address" type="email" />
+        </div>
+        <div className={styles.inputContainer}>
+          <label>Phone Number</label>
+          <input placeholder="Enter email address" type="text" />
+        </div>
+        {/* password */}
+        <div className={styles.passwordContainer}>
+          <label>Password</label>
+          <div className={styles.passwordInput}>
+            <input placeholder="Enter password" type="password" />
+            <Image
+              src={"/images/eye.svg"}
+              alt="visibility"
+              width={24}
+              height={24}
+            />
+          </div>
+        </div>
+        {/* user type */}
+        <div className={styles.passwordContainer}>
+          <label>I&apos;m signing up as</label>
+          <div className={styles.passwordInput}>
+            <input placeholder="Select" type="" />
+            <Image
+              src={"/images/eye.svg"}
+              alt="dropdown"
+              width={24}
+              height={24}
+            />
+          </div>
+        </div>
+        {/* submit button */}
+        <button className={styles.submitButton}>
+          <p>Create an Account</p>
+        </button>
+      </form>
+
+      {/* have an account? */}
+      <div className={styles.haveAnAccount}>
+        <p>I already have an account? </p>
+        <Link href={Routes.Login} className={styles.loginText}>
+          Log In
+        </Link>
+      </div>
     </div>
-  )
+  );
 }
 
 export default function Signup() {
